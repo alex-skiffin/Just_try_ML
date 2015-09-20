1c9e777fee4d94be0f7a2f4a64e3f31e3879ebe3
Alex Skiffin <alex@skiffin.ru>
2015-09-20 22:49:28 +0500

    some change

--- TryLearn.py
+++ TryLearn.py
@@ -2,16 +2,22 @@ import numpy as np
 import pandas
 
 
-train=pandas.read_csv("cleaned.csv")
-dataset = np.loadtxt(train, delimiter=",", dtype='str')
+train=pandas.read_csv("cleaned2.csv")
+#dataset = np.loadtxt(train, delimiter=",", dtype='str')
 # separate the data from the target attributes
-X = dataset[1:-1]
-Y = dataset[-1:]
-
+#print (len(dataset))
+#print (len(train))
+X = train[1:][1:-1]
+Y = train[-1:]
+#print(len(X))
+#from sklearn.naive_bayes import GaussianNB
+#gnb = GaussianNB()
+#y_pred = gnb.fit(X, Y)#.predict(X)
+#print(y_pred)
 
 from sklearn import preprocessing
 # normalize the data attributes
-normalized_X = preprocessing.normalize(X, axis=1)
+normalized_X = preprocessing.normalize(X)
 # standardize the data attributes
 #standardized_X = preprocessing.scale(X)
 
