8d05e5b8fde857bb10ade3f83be39970d6388e04
alex-skiffin <alex@skiffin.ru>
2015-09-25 15:15:42 +0500

    more logs, save to file and fix error

--- TryLearn.py
+++ TryLearn.py
@@ -5,10 +5,13 @@ print("load data")
 trainNP=numpy.genfromtxt("train.csv", delimiter=",", dtype="U75", skip_header=1)
 testNP=numpy.genfromtxt("test.csv", delimiter=",", dtype="U75", skip_header=1)
 testNP=testNP[:,1:]
+testNP[testNP == '']='0'
+print(len(testNP[0,:]))
+print(len(trainNP[0,:-1]))
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 #le.fit(trainNP[1])
-#print(le.transform(trainNP[1]))
+#print(le.transform(trainNP[1]))43973
 
 allDataForNormalize=trainNP[:,:-1]
 
@@ -30,12 +33,26 @@ while i <= (len(allDataForNormalize[0,:])-1):
 	print(le.transform(temp))
 	allDataForNormalize[:,i]=le.transform(temp[:50000])
 	testNP[:,i]=le.transform(temp[50000:])
+	print (testNP[:,i])
+	print (allDataForNormalize[:,i])
 	i=i+1
+
 	
-print (allDataForNormalize)
-testNP=allDataForNormalize[:50000,:]
-XData = allDataForNormalize[:50000,:-1]
-YTarget = allDataForNormalize[:50000,-1]
+	
+print (str(len(allDataForNormalize[0,:])) +'		'+str(len(allDataForNormalize[:,0])))
+print (str(len(testNP[0,:])) +'		'+str(len(testNP[:,0])))
+print ("save to file")
+numpy.savetxt("testNP.csv", testNP.astype(float), delimiter=",")
+numpy.savetxt("allDataForNormalize.csv", allDataForNormalize.astype(float), delimiter=",")
+XData = allDataForNormalize[:50000,:]
+#numpy.nan_to_num(testNP)#.fillna(0)
+YTarget = trainNP[:,-1]
+#allDataForNormalize[:50000,-1]
+print (str(len(allDataForNormalize[0,:])) +'		'+str(len(allDataForNormalize[:,0])))
+print (str(len(XData[0,:])) +'		'+str(len(XData[:,0])))
+print (str(len(YTarget)) +'		'+str(len(YTarget)))
+print (str(len(testNP[0,:])) +'		'+str(len(testNP[:,0])))
+print (testNP[-1,:])
 
 print("fit clf data")
 from sklearn import svm
@@ -43,8 +60,17 @@ clf = svm.SVC(gamma=0.001, C=100.)
 clf.fit(XData, YTarget)
 print("predict clf data")
 testCLFResult=clf.predict(testNP)
+result=[[0 for x in range(2)] for x in range(63)] 
+result[0][0]="ID"
+result[0][1]="y"
+i=1
+for el in testCLFResult:
+	result[i][1]=el
+	result[i][0]=i-1
+	i=i+1
 
-numpy.savetxt("CLFresult.csv", testCLFResult, delimiter=",")
+numpy.savetxt("CLFresult.csv", testCLFResult.astype(float), delimiter=",")
+numpy.savetxt("CLFresult2.csv", result.astype(float), delimiter=",")
 #print(len(X))
 #from sklearn.naive_bayes import GaussianNB
 #gnb = GaussianNB()
@@ -54,7 +80,7 @@ numpy.savetxt("CLFresult.csv", testCLFResult, delimiter=",")
 print("normalized_X")
 from sklearn import preprocessing
 # normalize the data attributes
-normalized_X = preprocessing.normalize(XData)
+normalized_X = preprocessing.normalize(XData.astype(float))
 # standardize the data attributes
 #standardized_X = preprocessing.scale(X)
 
@@ -71,4 +97,4 @@ predicted = model.predict(testNP)
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
 
-numpy.savetxt("GaussianNBresult.csv", predicted, delimiter=",")
+numpy.savetxt("GaussianNBresult.csv", predicted.astype(float), delimiter=",")
